fa2d417139900e4862b65f290a47cc0fc3c947c5
Liya208 <2718411049@qq.com>
2026-05-26 12:47:09 +0000

    Deploying to gh-pages from @ Liya208/YingweiWang@d0bcd088f9111897712c8b4d17a7e4f62dc27a52 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-research-areas",
+          title: 'Research Areas',
+          description: "Schematic Diagram of the Research Group&#39;s Research Focus",
           section: "Projects",handler: () => {
               window.location.href = "/YingweiWang/projects/1_project/";
             },},{id: "projects-project-2",
