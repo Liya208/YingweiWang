3865efa5d274ad5a7d07a0f39a4b4b0e902b0af1
Liya208 <2718411049@qq.com>
2026-05-27 06:22:30 +0000

    Deploying to gh-pages from @ Liya208/YingweiWang@8eafc38e56ac44cb2e88dc13eafb9cbf08f7d605 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Focusing on the field of micro- and nano-optics, the group conducts basic and applied basic research on the interaction between light and matter. It has achieved a series of key breakthroughs in areas such as the spectral characterization of nonlinear optical effects and ultrafast dynamical processes in two-dimensional semiconductors, as well as the control of light fields on metamaterial surfaces and the development of novel ultra-thin optical modulators.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/YingweiWang/cv/";
