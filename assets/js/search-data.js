421e9ad60595ac9a2343699c1944b047f2762305
Liya208 <2718411049@qq.com>
2026-06-05 13:06:29 +0000

    Deploying to gh-pages from @ Liya208/YingweiWang@86a50bcb798b1c164274140a26983dc2f860972a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,6 +51,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/YingweiWang/teaching/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/YingweiWang/people/";
+          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
